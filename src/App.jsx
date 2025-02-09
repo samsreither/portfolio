108e491a5ca8f3a7a215104b85136a6fa5f317c8
Sam Reither <samsreither@gmail.com>
2025-02-09 09:29:10 -0500

    working on contact page

--- src/App.jsx
+++ src/App.jsx
@@ -6,6 +6,7 @@ import { MobileMenu } from "./components/MobileMenu";
 import { Home } from "./components/sections/home";
 import { About } from "./components/sections/About";
 import { Projects } from "./components/sections/Projects";
+import { Contact } from "./components/sections/Contact";
 import "./index.css";
 
 function App() {
@@ -22,6 +23,7 @@ function App() {
         <Home />
         <About />
         <Projects />
+        <Contact />
       </div>
     </>
   );
