a907e8c1cbb40b6c1330b499775eadddb00b6ee9
Sam Reither <samsreither@gmail.com>
2025-02-08 20:38:52 -0500

    working on contact page

--- src/components/sections/About.jsx
+++ src/components/sections/About.jsx
@@ -1,3 +1,5 @@
+import { RevealOnScroll } from "../RevealOnScroll";
+
 export const About = () => {
   const frontendSkills = ["React", "Vue", "Typescript", "TailwindCSS"];
   const backendSkills = ["Node.js", "Python", "AWS", "MongoDB", "GraphQL"];
@@ -7,32 +9,88 @@ export const About = () => {
       id="about"
       className="min-h-screen flex items-center justify-center py-20"
     >
-      <div className="max-w-3xl mx-auto px-4">
-        <h2 className="text-3xl font-bold mb-8 bg-gradient-to-r from-blue-500 to-cyan-400 bg-clip-text text-transparent text-center">
-          About Me
-        </h2>
-        <div className="rounded-xl p-8 border-white/10 border hover:-translate-y-1 transition-all">
-          <p className="text-gray-300 mb-6">
-            Developer with expertise building web apps. Former Math Teacher &
-            Actuary.
-          </p>
-        </div>
-        <div className="grid grid-cols-1 md:grid-cols-2 gap-6">
-          <div className="rounded-xl p-6 hover:-translate-y-1 transition-all">
-            <h3 className="text-xl font-bold mb-4">Frontend</h3>
-            <div className="flex flex-wrap gap-2">
-              {frontendSkills.map((tech, key) => (
-                <span
-                  key={key}
-                  className="bg-blue-500/10 text-blue-500 py-1 px-3 rounded-full text-sm hover:bg-blue-500/20 hover:shadow-[0_2px_8px_rgba(59,130,2246,0.2)] transition"
-                >
-                  {tech}
-                </span>
-              ))}
+      <RevealOnScroll>
+        <div className="max-w-3xl mx-auto px-4">
+          <h2 className="text-3xl font-bold mb-8 bg-gradient-to-r from-blue-500 to-cyan-400 bg-clip-text text-transparent text-center">
+            About Me
+          </h2>
+          <div className="rounded-xl p-8 border-white/10 border hover:-translate-y-1 transition-all">
+            <p className="text-gray-300 mb-6">
+              Developer with expertise building web apps. Former Math Teacher &
+              Actuary.
+            </p>
+          </div>
+          <div className="grid grid-cols-1 md:grid-cols-2 gap-6">
+            <div className="rounded-xl p-6 hover:-translate-y-1 transition-all">
+              <h3 className="text-xl font-bold mb-4">Frontend</h3>
+              <div className="flex flex-wrap gap-2">
+                {frontendSkills.map((tech, key) => (
+                  <span
+                    key={key}
+                    className="bg-blue-500/10 text-blue-500 py-1 px-3 rounded-full text-sm hover:bg-blue-500/20 hover:shadow-[0_2px_8px_rgba(59,130,246,0.2)] transition"
+                  >
+                    {tech}
+                  </span>
+                ))}
+              </div>
+            </div>
+
+            <div className="rounded-xl p-6 hover:-translate-y-1 transition-all">
+              <h3 className="text-xl font-bold mb-4">Backend</h3>
+              <div className="flex flex-wrap gap-2">
+                {backendSkills.map((tech, key) => (
+                  <span
+                    key={key}
+                    className="bg-blue-500/10 text-blue-500 py-1 px-3 rounded-full text-sm hover:bg-blue-500/20 hover:shadow-[0_2px_8px_rgba(59,130,2246,0.2)] transition"
+                  >
+                    {tech}
+                  </span>
+                ))}
+              </div>
+            </div>
+          </div>
+
+          <div className="grid grid-cols-1 md:grid-cols-2 gap-6 mt-8">
+            <div className="p-6 rounded-xl border-white/10 border hover:-translate-y-1 transition-all">
+              <h3 className="text-xl font-bold mb-4"> Education </h3>
+              <ul className="list-disc list-inside text-gray-300 space-y-2">
+                <li>
+                  <strong> B.S. in Mathematics </strong> - University of
+                  Pittsburgh (2011-2015)
+                </li>
+                <li>Tripleten Software Engineering Bootcamp - (2024)</li>
+              </ul>
+            </div>
+            <div className="p-6 rounded-xl border-white/10 border hover:-translate-y-1 transition-all">
+              <h3 className="text-xl font-bold mb-4"> Work Experience </h3>
+              <div className="space-y-4 text-gray-300">
+                <div>
+                  <h4 className="font-semibold">
+                    Software Developer at Sam and Kels Adventures (2024 -
+                    Present)
+                  </h4>
+                  <p> Developed and maintained all aspects of the website. </p>
+                </div>
+                <div>
+                  <h4 className="font-semibold">
+                    Math & Physics Teacher (2017 - 2024)
+                  </h4>
+                  <p>
+                    {" "}
+                    Taught courses ranging from Pre-Algebra to Pre-Calculus.{" "}
+                  </p>
+                </div>
+                <div>
+                  <h4 className="font-semibold">
+                    Actuary at Ernst & Young LLC (2015 - 2017)
+                  </h4>
+                  <p> Audited actuarial assumptions of clients. </p>
+                </div>
+              </div>
             </div>
           </div>
         </div>
-      </div>
+      </RevealOnScroll>
     </section>
   );
 };
---
+++ src/components/sections/Contact.jsx
@@ -0,0 +1,5 @@
+export const Contact = () => {
+    return <section id="contact" className="min-h-screen flex items-center justify-center py-20">
+
+    </section>
+}
